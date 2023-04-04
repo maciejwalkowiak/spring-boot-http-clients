7a5f5e7e9cdfede34f7dfd8dff400dd61f9e8137
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2023-04-04 21:56:39 +0200

    polish

--- example/src/main/java/app/App.java
+++ example/src/main/java/app/App.java
@@ -2,12 +2,8 @@ package app;
 
 import java.util.List;
 
-import com.maciejwalkowiak.spring.http.annotation.EnableHttpClients;
-
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,13 +12,6 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Bean
-    ApplicationRunner applicationRunner(TodoClient todoClient) {
-        return args -> {
-            System.out.println(todoClient.get());
-        };
     }
 }
 
